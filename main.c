1283177df669bbccf6611644ea3a9b193fcd4985
MSchalken <m.m.r.schalken@gmail.com>
2023-11-30 12:26:46 +0100

    Add common.h

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#include "src/common.h"
+
 int main(int argc, const char *argv[])
 {
     fprintf(stderr, "Usage: clox [path]\n");
---
+++ src/common.h
@@ -0,0 +1,10 @@
+#if !defined(clox_common_h)
+#define clox_common_h
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#define DEBUG_TRACE_EXECUTION
+
+#endif // clox_common_h
