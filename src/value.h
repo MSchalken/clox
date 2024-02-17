df9eb2640ceca1a5d9c882419e8fe81044399715
MSchalken <m.m.r.schalken@gmail.com>
2024-02-17 22:47:25 +0100

    Add clang-format and format files

--- src/value.h
+++ src/value.h
@@ -6,23 +6,15 @@
 typedef struct Obj Obj;
 typedef struct ObjString ObjString;
 
-typedef enum
-{
-    VAL_NIL,
-    VAL_BOOL,
-    VAL_NUMBER,
-    VAL_OBJ
-} ValueType;
+typedef enum { VAL_NIL, VAL_BOOL, VAL_NUMBER, VAL_OBJ } ValueType;
 
-typedef struct
-{
-    ValueType type;
-    union
-    {
-        bool boolean;
-        double number;
-        Obj * obj;
-    } as;
+typedef struct {
+  ValueType type;
+  union {
+    bool boolean;
+    double number;
+    Obj *obj;
+  } as;
 } Value;
 
 #define IS_BOOL(value) ((value).type == VAL_BOOL)
@@ -34,23 +26,22 @@ typedef struct
 #define AS_NUMBER(value) ((value).as.number)
 #define AS_OBJ(value) ((value).as.obj)
 
-#define BOOL_VAL(value) ((Value) {VAL_BOOL, {.boolean = value}})
-#define NIL_VAL ((Value) {VAL_NIL, {.number = 0}})
-#define NUMBER_VAL(value) ((Value) {VAL_NUMBER, {.number = value}})
-#define OBJ_VAL(object) ((Value) {VAL_OBJ, {.obj = (Obj*)object}})
+#define BOOL_VAL(value) ((Value){VAL_BOOL, {.boolean = value}})
+#define NIL_VAL ((Value){VAL_NIL, {.number = 0}})
+#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
+#define OBJ_VAL(object) ((Value){VAL_OBJ, {.obj = (Obj *)object}})
 
-typedef struct
-{
-    int capacity;
-    int count;
-    Value * values;
+typedef struct {
+  int capacity;
+  int count;
+  Value *values;
 } ValueArray;
 
-bool valuesEqual( Value a, Value b );
+bool valuesEqual(Value a, Value b);
 
-void initValueArray( ValueArray * array );
-void writeValueArray( ValueArray * array, Value value );
-void freeValueArray( ValueArray * array );
-void printValue( Value value );
+void initValueArray(ValueArray *array);
+void writeValueArray(ValueArray *array, Value value);
+void freeValueArray(ValueArray *array);
+void printValue(Value value);
 
-#endif // clox_value_h
+#endif  // clox_value_h
